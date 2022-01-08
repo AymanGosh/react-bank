560cc5540675cc2afd1acbbd65753dfb529c2e20
aymanab5 <aymansce@gmail.com>
2022-01-08 22:05:59 +0200

    display it in red if it is below 500 else  green

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import "../App.css";
 
 export default class Balance extends Component {
   render() {
+    let nameOfClass;
+    if (this.props.balance > 500) {
+      nameOfClass = "green";
+    } else {
+      nameOfClass = "red";
+    }
     return (
       <div>
-        <p> Balance : {this.props.balance}</p>
+        <p className={nameOfClass}> Balance : {this.props.balance}</p>
       </div>
     );
   }
